e7145b2a981e4430728c81913f2fd613bfe46b05
shengchuang-wong <shengchuang1996@gmail.com>
2020-04-25 19:01:14 +0800

    add documentation for basic enumerable methods

---
+++ basic-enumerable-methods.rb
@@ -0,0 +1,166 @@
+=begin
+Enumerables are a set of convenient built-in methods 
+in Ruby that are included as part of both arrays and hashes.
+=end
+
+# Life Before Enumerables
+friends = ['Sharon', 'Leo', 'Leila', 'Brian', 'Arun']
+invited_list = []
+
+for friend in friends do
+  if friend != 'Brian'
+  invited_list.push(friend)
+  end
+end
+
+invited_list #=> ["Sharon", "Leo", "Leila", "Arun"]
+
+# #select
+friends = ['Sharon', 'Leo', 'Leila', 'Brian', 'Arun']
+
+friends.select { |friend| friend != 'Brian' }
+#=> ["Sharon", "Leo", "Leila", "Arun"]
+
+friends = ['Sharon', 'Leo', 'Leila', 'Brian', 'Arun']
+
+friends.reject { |friend| friend == 'Brian' }
+#=> ["Sharon", "Leo", "Leila", "Arun"]
+
+# #each
+friends = ['Sharon', 'Leo', 'Leila', 'Brian', 'Arun']
+
+friends.each { |friend| puts "Hello, " + friend }
+
+#=> Hello, Sharon
+#=> Hello, Leo
+#=> Hello, Leila
+#=> Hello, Brian
+#=> Hello, Arun
+
+#=> ["Sharon", "Leo", "Leila", "Brian" "Arun"]
+
+# mutliple line
+my_array = [1, 2]
+
+my_array.each do |num|
+  num *= 2
+  puts "The new number is #{num}."
+end
+
+# each for hashes
+my_hash = { "one" => 1, "two" => 2 }
+
+my_hash.each { |key, value| puts "#{key} is #{value}" }
+
+one is 1
+two is 2
+#=> { "one" => 1, "two" => 2}
+
+my_hash.each { |pair| puts "the pair is #{pair}" }
+
+the pair is ["one", 1]
+the pair is ["two", 2]
+#=> { "one" => 1, "two" => 2}
+
+# each always return the original array
+friends = ['Sharon', 'Leo', 'Leila', 'Brian', 'Arun']
+
+friends.each { |friend| friend.upcase }
+
+#=> ['Sharon', 'Leo', 'Leila', 'Brian', 'Arun']
+
+# #each_with_index
+fruits = ["apple", "banana", "strawberry", "pineapple"]
+
+fruits.each_with_index { |fruit, index| puts fruit if index.even? }
+
+#=> apple
+#=> strawberry
+#=> ["apple", "banana", "strawberry", "pineapple"]
+
+# #map
+friends = ['Sharon', 'Leo', 'Leila', 'Brian', 'Arun']
+
+friends.map { |friend| friend.upcase }
+#=> `['SHARON', 'LEO', 'LEILA', 'BRIAN', 'ARUN']`
+
+my_order = ['medium Big Mac', 'medium fries', 'medium milkshake']
+
+my_order.map { |item| item.gsub('medium', 'extra large') }
+#=> ["extra large Big Mac", "extra large fries", "extra large milkshake"]
+
+salaries = [1200, 1500, 1100, 1800]
+
+salaries.map { |salary| salary - 700 }
+#=> [500, 800, 400, 1100]
+
+# #select
+friends = ['Sharon', 'Leo', 'Leila', 'Brian', 'Arun']
+
+friends.select { |friend| friend != 'Brian' }
+
+ #=> ["Sharon", "Leo", "Leila", "Arun"]
+
+responses = { 'Sharon' => 'yes', 'Leo' => 'no', 'Leila' => 'no', 'Arun' => 'yes' }
+responses.select { |person, response| response == 'yes'}
+#=> {"Sharon"=>"yes", "Arun"=>"yes"}
+
+my_numbers = [5, 6, 7, 8]
+
+my_numbers.reduce { |sum, number| sum + number }
+#=> 26
+
+my_numbers = [5, 6, 7, 8]
+
+my_numbers.reduce(1000) { |sum, number| sum + number } #=> 1000 is default value
+#=> 1026
+
+votes = ["Bob's Dirty Burger Shack", "St. Mark's Bistro", "Bob's Dirty Burger Shack"]
+
+votes.reduce(Hash.new(0)) do |result, vote|
+  result[vote] += 1
+  result
+end
+#=> {"Bob's Dirty Burger Shack"=>2, "St. Mark's Bistro"=>1}
+
+hundreds = Hash.new(100)
+hundreds["first"]         #=> 100
+hundreds["mine"]          #=> 100
+hundreds["yours"]         #=> 100
+
+hundreds = Hash.new(100)
+hundreds["new"]           #=> 100
+hundreds["new"] = 99
+hundreds["new"]           #=> 99
+
+# #Bang! #map and #select Methods - mutate original arrays
+friends = ['Sharon', 'Leo', 'Leila', 'Brian', 'Arun']
+
+friends.map! { |friend| friend.upcase }
+#=> `['SHARON', 'LEO', 'LEILA', 'BRIAN', 'ARUN']`
+
+friends
+#=> `['SHARON', 'LEO', 'LEILA', 'BRIAN', 'ARUN']`
+
+# Return Values of Enumerables
+friends = ['Sharon', 'Leo', 'Leila', 'Brian', 'Arun']
+
+invited_friends = friends.select { |friend| friend != 'Brian' }
+
+friends
+#=> ['Sharon', 'Leo', 'Leila', 'Brian', 'Arun']
+
+invited_friends
+#=> ["Sharon", "Leo", "Leila", "Arun"]
+
+friends = ['Sharon', 'Leo', 'Leila', 'Brian', 'Arun']
+
+def invited_friends(friends)
+  friends.select { |friend| friend != 'Brian' }
+end
+
+friends
+#=> ['Sharon', 'Leo', 'Leila', 'Brian', 'Arun']
+
+invited_friends(friends)
+ #=> ["Sharon", "Leo", "Leila", "Arun"]
